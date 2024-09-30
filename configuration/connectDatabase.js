d279dfd5bb3c48bcd551380cb75d41998e030866
AAbhishek1234 <er.abhishekrawat73@gmail.com>
2024-09-30 22:14:22 +0530

    dockerfile

--- configuration/connectDatabase.js
+++ configuration/connectDatabase.js
@@ -1,35 +1,19 @@
-// import mongoose from "mongoose";
-// const mongoUrl = process.env.DB_URI || "mongodb://127.0.0.1:27017/clg";
-// // console.log(process.env.DB_URI);
-
-// export const connectDatabase = () => {
-//   if (!mongoUrl) {
-//     return console.log("Mongo URI not provided!");
-//   }
-
-//   mongoose
-//     .connect(mongoUrl)
-//     .then(() => {
-//       console.log("Connected to MongoDB!!!!!");
-//     })
-//     .catch((error) => {
-//       console.error("Connection error", error);
-//     });
-// };
-
-import mongoose from 'mongoose';
-const mongoUrl = process.env.DB_URI; // Ensure this points to your cloud database URI
+import mongoose from "mongoose";
+const mongoUrl = process.env.DB_URI || "mongodb://127.0.0.1:27017/clg";
+// console.log(process.env.DB_URI);
 
 export const connectDatabase = () => {
-    if (!mongoUrl) {
-        return console.log("Mongo URI not provided!");
-    }
+  if (!mongoUrl) {
+    return console.log("Mongo URI not provided!");
+  }
 
-    mongoose.connect(mongoUrl)
-        .then(() => {
-            console.log("Connected to MongoDB!");
-        })
-        .catch((error) => {
-            console.error("Connection error", error);
-        });
+  mongoose
+    .connect(mongoUrl)
+    .then(() => {
+      console.log("Connected to MongoDB!!!!!");
+    })
+    .catch((error) => {
+      console.error("Connection error", error);
+    });
 };
+
