cddf3fe1c26f69cbdf9e3e8e8a331bc00caf733e
AAbhishek1234 <er.abhishekrawat73@gmail.com>
2024-10-01 21:58:24 +0530

    dbatlas

--- configuration/connectDatabase.js
+++ configuration/connectDatabase.js
@@ -1,14 +1,38 @@
+// import mongoose from "mongoose";
+// //const mongoUrl = process.env.DB_URI || "mongodb://127.0.0.1:27017/clg";
+// const mongoURL = "mongodb+srv://virajrawat50:abhishek@123cluster0.3ut9t.mongodb.net/"
+// // console.log(process.env.DB_URI);
+
+// export const connectDatabase = () => {
+//   if (!mongoURL) {
+//     return console.log("Mongo URI not provided!");
+//   }
+
+//   mongoose
+//     .connect(mongoURL)
+//     .then(() => {
+//       console.log("Connected to MongoDB!!!!!");
+//     })
+//     .catch((error) => {
+//       console.error("Connection error", error);
+//     });
+// };
+
 import mongoose from "mongoose";
-const mongoUrl = process.env.DB_URI || "mongodb://127.0.0.1:27017/clg";
-// console.log(process.env.DB_URI);
+
+// Use the correct connection string from MongoDB Atlas
+const mongoURL = "mongodb+srv://viraj:viraj123@cluster0.3ut9t.mongodb.net/";
 
 export const connectDatabase = () => {
-  if (!mongoUrl) {
+  if (!mongoURL) {
     return console.log("Mongo URI not provided!");
   }
 
   mongoose
-    .connect(mongoUrl)
+    .connect(mongoURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
     .then(() => {
       console.log("Connected to MongoDB!!!!!");
     })
@@ -16,4 +40,3 @@ export const connectDatabase = () => {
       console.error("Connection error", error);
     });
 };
-
